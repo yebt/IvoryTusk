daf65d9ebbbdb02d6cf6b3ca4f85540cfe0545aa
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-05-09 16:21:52 -0500

    move all to .old

--- old_bin/commands/hello_word.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-# --------------------------------------------------------
-
-export COMMAND_HELP_TEXT="This command print a 'hello word' message"
-export ARGUMENTS=()
-export FLAGS=()
-
-# --------------------------------------------------------
